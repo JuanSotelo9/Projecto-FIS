aed1a8e97138a2d53f181d364394fdd16050481c
AndresSalazar <andressalazar062858@gmail.com>
2024-04-19 00:45:42 -0500

    Se añadio vision y mision

--- Intento frond/script/script.js
+++ Intento frond/script/script.js
@@ -10,6 +10,7 @@ function mover() {
         // Mostrar los botones "Iniciar sesión" y "Registrarse"
         document.getElementById('Iniciar').style.opacity = '1';
         document.getElementById('Registrar').style.opacity = '1';
+        document.getElementById('MisionVision').style.opacity = '1';
         document.getElementById('Iniciar').style.pointerEvents = 'auto';
         document.getElementById('Registrar').style.pointerEvents = 'auto';
     }, 2000);
@@ -27,3 +28,20 @@ function intentoregistrar() {
     window.location.href = '#modal';
 }
 document.getElementById('Registrar').addEventListener('click', intentoregistrar);
+
+// Animacion de cambio de texto
+window.onload = function() {
+    var missionText = "<strong>Misión:</strong> <br> La Universidad Distrital Francisco José de Caldas es un espacio social y una organización institucional, ente autónomo del orden distrital, que tiene entre sus finalidades la formación de profesionales especializados y de ciudadanos activos; la producción y reproducción del conocimiento científico, además de la innovación tecnológica y la creación artística. Impulsa el diálogo de saberes y promueve una pedagogía, capaz de animar la reflexión y la curiosidad de los estudiantes; además, fomenta un espíritu crítico en la búsqueda de verdades abiertas; en la promoción de la ciencia y la creación; asimismo, de la ciudadanía y la democracia; y alienta la deliberación, fundada en la argumentación y en el diálogo razonado.";
+    var visionText = "<strong>Visión:</strong> <br> Para el 2030 la Universidad Distrital Francisco José de Caldas será reconocida, nacional e internacionalmente, como una institución de alta calidad en la formación de ciudadanos responsables y profesionales del mejor nivel, en la producción de conocimiento científico, artístico y de innovación tecnológica; propósitos que desplegará en los campos de la docencia, la investigación y la extensión.";
+    var container = document.getElementById("ContenedorPoliticadecalidad");
+    var isMission = true;
+    setInterval(function() {
+        if (isMission) {
+            container.innerHTML = "<div id='Politicadecalidad' class='Politicadecalidad'>" + visionText + "</div>";
+            isMission = false;
+        } else {
+            container.innerHTML = "<div id='Politicadecalidad' class='Politicadecalidad'>" + missionText + "</div>";
+            isMission = true;
+        }
+    }, 120000);
+};
