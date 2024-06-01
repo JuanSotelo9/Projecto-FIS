928e971c262806e6b0ce3f2d4fdbf15280ff5f12
JuanSotelo9 <jpsotelor@udistrital.edu.co>
2024-06-01 10:41:58 -0500

    Actualizacion periodica estados y disponibilidad

--- backend/src/main/java/com/api/backend/repository/PoseerRepository.java
+++ backend/src/main/java/com/api/backend/repository/PoseerRepository.java
@@ -1,5 +1,7 @@
 package com.api.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +19,7 @@ public interface PoseerRepository extends JpaRepository<Poseer, PoseerId>{
     @Modifying
     @Query("DELETE from Poseer r WHERE r.poseerId.kIdrecurso = :kIdrecurso AND r.poseerId.kIddisponibilidad = :kIddisponibilidad")
     public void deleteDisponibilidad(@Param("kIdrecurso") int kIdrecurso, @Param("kIddisponibilidad") int kIddisponibilidad);
+
+    @Query("SELECT r from Poseer r WHERE r.poseerId.kIddisponibilidad = :kIddisponibilidad")
+    public List<Poseer> findBykIddisponibilidad(@Param("kIddisponibilidad") int kIddisponibilidad);
 }
