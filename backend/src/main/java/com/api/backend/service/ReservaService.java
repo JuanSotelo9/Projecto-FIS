67caf9b1587ee7a6706492264265d008f3d7dc74
JuanSotelo9 <jpsotelor@udistrital.edu.co>
2024-05-31 19:55:12 -0500

    Creacion servicio cancelar reserva

--- backend/src/main/java/com/api/backend/service/ReservaService.java
+++ backend/src/main/java/com/api/backend/service/ReservaService.java
@@ -1,8 +1,13 @@
 package com.api.backend.service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -17,7 +22,9 @@ import lombok.RequiredArgsConstructor;
 public class ReservaService {
     
     private final ReservaRepository reservaRepository;
+    private final RecursoService recursoService;
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+
 
     public boolean reservarRecurso(ReservarRequest request){
         Reserva reserva = new Reserva();
@@ -25,7 +32,7 @@ public class ReservaService {
         reserva.setFFechareserva(request.getDia());
         reserva.setFHorafinalreserva(request.getHoraFinal());
         reserva.setFHorainicioreserva(request.getHoraInicio());
-        reserva.setNEstadoreserva("Reservado");
+        reserva.setNEstadoreserva("reservado");
         reserva.setKIdusuario(request.getIdUsuario());
         reserva.setKIdrecurso(request.getIdRecurso());
         try {
@@ -43,5 +50,34 @@ public class ReservaService {
 
     public List<Reserva> getReservas(Long idUser){
         return reservaRepository.findBykIdusuario(idUser);
+    }
+
+    public String cancelarReserva(String idReserva){
+        Optional<Reserva> reserva = reservaRepository.findById(idReserva);
+        if(reserva.isPresent()){
+            if(reserva.get().getNEstadoreserva().equals("reservado")){
+
+                LocalDateTime now = LocalDateTime.now();
+                LocalDate fecha = new java.util.Date(reserva.get().getFFechareserva().getTime()).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                LocalTime hora = reserva.get().getFHorainicioreserva().toLocalTime();
+                LocalDateTime fechaHora = LocalDateTime.of(fecha, hora);
+                if(ChronoUnit.HOURS.between(now, fechaHora) < 2){
+                    return "fuera de plazo";
+                }else{
+                    reserva.get().setNEstadoreserva("cancelado");
+                    reservaRepository.save(reserva.get());
+                    int idDisponibilidad = recursoService.getIdDisponibilidad(reserva.get().getFFechareserva(), reserva.get().getFHorainicioreserva());
+                    recursoService.crearRecursoDisponibilidad(reserva.get().getKIdrecurso(), idDisponibilidad);
+                    return "cancelado";
+                }
+                
+            }else{
+                return "reserva no esta en estado reservado";
+            }
+        }else{
+            return "reserva no existe";
+        }
+        
+        
     }
 }
